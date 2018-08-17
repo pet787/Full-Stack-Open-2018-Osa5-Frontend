326947ac95bcf00bc67001da5dc2cf079f7b736f
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-17 17:39:00 +0300

    5.1

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "no-console": 0,
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "globals": {
+        }
+
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import Notification from './components/Notification'
+import loginService from './services/login'
 import blogService from './services/blogs'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null
     }
   }
 
@@ -16,15 +22,56 @@ class App extends React.Component {
     )
   } 
 
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      this.setState({ username: '', password: '', user })
+    } catch (exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
-    return (
-      <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
-      </div>
-    );
+    if (this.state.user === null) {
+      return (
+        <div>
+           <Notification message={this.state.error} />
+           <LoginForm
+              username={this.state.username}
+              password={this.state.password}
+              handleChange={this.handleLoginFieldChange}
+              handleSubmit={this.login}
+            />
+        </div>
+      )
+     } else {
+      return (
+        <div>
+          <Notification message={this.state.error} />
+          <p>{this.state.user.name} logged in</p>
+          <h2>blogs</h2>
+          {this.state.blogs.map(blog =>
+            <Blog key={blog._id} blog={blog} />
+          )}
+        </div>
+      )
+    }
   }
 }
 
